92872d8072b18cec8d2c368719aed73f560a46bb
Mirat Can Bayrak <miratcanbayrak@gmail.com>
2023-08-18 18:28:09 +0300

    Rename url params

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 export const RAW_GITHUB_CONTENT_BASE_URL = "https://raw.githubusercontent.com";
 export const TARGET_FILENAME = "sites.json";
 
-export const URL_REPO_PARAM = "repo";
-export const URL_DOMAIN_PARAM = "domain";
-export const URL_ACTION_PARAM = "action";
+export const REPO_PARAM = "repo";
+export const REF_PARAM = "ref";
+export const ACTION_PARAM = "action";
